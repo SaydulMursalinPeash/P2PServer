0b02a069e2845f5a9669771e92ec50666a5c5064
SaydulMursalinPeash <saydulpeash428@gmail.com>
2023-06-14 05:04:09 +0600

    jhgv

--- P2P/settings.py
+++ P2P/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%7n2c*#4h(5y*j6q+u@%2=t)ezwz57&asfu9mc=$us&d=mm8nk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
